7c9a8f5d4da551a290e5f025b74172bff40786cc
Shaohua Zhou <sh.zhou@gmail.com>
2013-05-07 21:51:07 -0700

    fix invoke() part1, call by string

--- src/underbar.js
+++ src/underbar.js
@@ -131,9 +131,16 @@ var _ = {};
 
   // Calls the method named by methodName on each value in the list.
   _.invoke = function(list, methodName) {
-    return _.map(list, function(value){
-      return methodName.call(value);
-    });
+    if(typeof methodName === "function"){
+      return _.map(list, function(value){
+        return methodName.call(value);
+      });
+    }else if(typeof methodName === "string"){
+      return _.map(list, function(value){
+        return value[methodName]();
+      });      
+    }
+
   };
 
   // Reduces an array or object to a single value by repetitively calling
